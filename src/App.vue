7abb2bdf6549c77c243de7d0f4da020dd25ebaa7
Super-Backend-Dev <ballbigy654@hotmail.co.th>
2024-08-10 20:30:20 +0700

    อัพเดต สุดเปอร์บอล

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,30 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="min-h-full">
+    <header class="shadow bg-[#c70101]">
+      <div class="flex justify-between px-4 py-6 mx-auto items-center max-w-7xl sm:px-6 lg:px-8">
+        <ul>
+          <li>
+            <router-link to="/" class="text-2xl tracking-tight text-white">OFFLINE</router-link>
+          </li>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        </ul>
+        <div>
+          <router-link to="/register" type="button" class="p-2  text-white bg-[#1a1a1a] border-spacing-2 rounded-md">
+            Register
+          </router-link>
+        </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+      </div>
+    </header>
+    <main>
+      <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <router-view></router-view>
+      </div>
+    </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div class="sm:columns-2 mb-2">
+      <img class="w-full hover:-translate-y-3 ease-out duration-300 shadow-xl aspect-video object-contain"
+        src="https://assets.manutd.com/AssetPicker/images/0/0/20/88/1333340/21-Antony1719824409448.png" />
+      <img class="w-full hover:-translate-y-3  shadow-xl ease-out duration-300 aspect-video object-contain"
+        src="https://assets.manutd.com/AssetPicker/images/0/0/20/88/1333356/5-Harry-Maguire1719821814914.png" />
+    </div>
+    <div class="sm:columns-2 ">
+      <img class="w-full hover:-translate-y-3 ease-out duration-300 shadow-xl aspect-video object-contain"
+        src="https://assets.manutd.com/AssetPicker/images/0/0/20/88/1333339/17-Alejandro-Garnacho1719824310390.png" />
+      <img class="w-full shadow-xl hover:-translate-y-3 ease-out duration-300 aspect-video object-contain"
+        src="https://assets.manutd.com/AssetPicker/images/0/0/20/88/1333350/37-Kobbie-Mainoo1719823470652.png" />
+    </div>
+  </div>
+</template>
